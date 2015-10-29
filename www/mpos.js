28c5bf3a3e00ecbc72553fdd03cf60e9bc9bce09
chongzi <mrchongzi@gmail.com>
2015-10-29 15:55:05 +0800

    js加上[]

--- www/mpos.js
+++ www/mpos.js
@@ -1,6 +1,4 @@
-
 var exec = require('cordova/exec');
-module.exports = exec(
-	function(a_){alert(a_);},null,"val_","val_1_");
+module.exports = exec(null,null,"val_","val_1_",[]);
  
 //module.exports = { show: function(content, type){ exec(null,null,"Toast","show",[content,type]); } }
