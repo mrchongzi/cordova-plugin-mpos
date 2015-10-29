b7f8a3992a01f9170fba28e7c6102a2fd4883751
chongzi <mrchongzi@gmail.com>
2015-10-29 16:22:34 +0800

    用网上的例子

--- src/android/mpos.java
+++ src/android/mpos.java
@@ -1,29 +1,27 @@
-package org.apache.cordova.device;
+package me.hran.cordova.toast;
 
-import java.util.TimeZone;
-
-import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import android.provider.Settings;
-public class mpos extends CordovaPlugin
-{
-	@Override
-public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    super.initialize(cordova, webView);
-    // your init code here
-}
-   
+/**
+ * Created by Hran on 2015/4/9.
+ */
+public class ToastPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        
-            callbackContext.success();
-            return true;
-         // Returning false results in a "MethodNotFound" error.
+        if ("show".equals(action)) {
+            showToast(args.getString(0), args.getInt(1));
+        }
+        return super.execute(action, args, callbackContext);
+    }
+
+    private void showToast(String text, int type) {
+        if (type == 1) {
+            android.widget.Toast.makeText(cordova.getActivity(), text, android.widget.Toast.LENGTH_LONG).show();
+        } else {
+            android.widget.Toast.makeText(cordova.getActivity(), text, android.widget.Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- www/mpos.js
+++ www/mpos.js
@@ -1,12 +1,10 @@
-var exec = require('cordova/exec');
-
-var splashscreen = {
-    show:function() {
-        exec(null, null, "SplashScreen", "show", []);
-    },
-    hide:function() {
-        exec(null, null, "SplashScreen", "hide", []);
+var exec = require("cordova/exec");
+    module.exports = {
+        show: function(content, type){
+            //参数1：成功回调function
+            //参数2：失败回调function
+            //参数3：feature name，与config.xml中注册的一致
+            //参数4：要传递的参数，json格式
+            exec(null,null,"Toast","show",[content,type]);
+        }
     }
-};
-
-module.exports = splashscreen;
