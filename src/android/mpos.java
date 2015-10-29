8357be88919d18254d091a6be048aa68b684903c
chongzi <mrchongzi@gmail.com>
2015-10-29 15:13:05 +0800

    update java

--- src/android/mpos.java
+++ src/android/mpos.java
@@ -1,10 +1,23 @@
+package org.apache.cordova.device;
+
+import java.util.TimeZone;
+
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.provider.Settings;
 public class mpos extends CordovaPlugin
 {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException 
     {
         webView.postMessage("splashscreen", "hide");
-        callbackContext.success();
+        callbackContext.success("haha");
         return true;
     }
 }
