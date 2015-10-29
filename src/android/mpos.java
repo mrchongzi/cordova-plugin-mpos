ba3bbbe11fa857d77b151a094cf6cbe6cbab809f
chongzi <mrchongzi@gmail.com>
2015-10-29 14:41:05 +0800

    add

---
+++ src/android/mpos.java
@@ -0,0 +1,10 @@
+public class pd extends CordovaPlugin 
+{
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException 
+    {
+        webView.postMessage("splashscreen", "hide");
+        callbackContext.success();
+        return true;
+    }
+}
---
+++ www/mpos.js
@@ -0,0 +1,6 @@
+alert("plugins.pd");
+var exec = require('cordova/exec');
+//module.exports = exec();
+ 
+module.exports = { show: function(content, type){ 
+	exec(null,null,"Toast","show",[content,type]); } }
