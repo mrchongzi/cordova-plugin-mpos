91b1aefdbbba0dd9cd9d52658a9a1e75529bfc92
chongzi <mrchongzi@gmail.com>
2015-10-29 15:40:34 +0800

    exec.success

--- www/mpos.js
+++ www/mpos.js
@@ -1,6 +1,6 @@
 
 var exec = require('cordova/exec');
-//module.exports = exec();
+module.exports = exec(
+	function(a_){alert(a_);},null,"val_","val_1_");
  
-module.exports = { show: function(content, type){ 
-	exec(null,null,"Toast","show",[content,type]); } }
+//module.exports = { show: function(content, type){ exec(null,null,"Toast","show",[content,type]); } }
